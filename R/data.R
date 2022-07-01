802718a93226adfbffa9e4e821f3f8985f10bf83
kylebennison <kylebeni012@gmail.com>
2022-06-30 23:14:27 -0400

    document datasets

---
+++ R/data.R
@@ -0,0 +1,72 @@
+#' List of power five conference names historically.
+#'
+#' @format a character vector
+#'
+#' @source created
+"power_5"
+
+#' List of group of five conference names historically.
+#'
+#' @format a character vector
+#'
+#' @source created
+"group_of_5"
+
+#' List of all scrimmage play types
+#'
+#' This contains the play names typically used in the pbp
+#' data for scrimmage plays. This does not include special teams plays
+#' like kickoffs or punts.
+#'
+#' @format a character vector:
+#'
+#'
+#' @source cfbd pbp data
+"scrimmage_plays_all"
+
+#' List of all plays that don't result in a turnover
+#'
+#' @format a character vector
+#'
+#' @source created
+"scrimmage_plays_non_turnover"
+
+#' List of all plays that DO result in a turnover
+#'
+#' This contains play types such as interceptions and fumbles.
+#' This does NOT include punts.
+#'
+#' @format a character vector
+#'
+#' @source created
+"scrimmage_plays_turnover"
+
+#' List of all passing play types
+#'
+#' @format a character vector
+#'
+#' @source created
+"scrimmage_plays_pass"
+
+#' List of all rushing play types
+#'
+#' @format a character vector
+#'
+#' @source created
+"scrimmage_plays_rush"
+
+#' List of all play types where no play occurs (timeouts, end of quarters, ...)
+#'
+#' @format a character vector
+#'
+#' @source created
+"no_action_plays"
+
+#' List of all plays that result in a kick
+#'
+#' @format a character vector
+#'
+#' @source created
+"scrimmage_plays_kicks"
+
+
