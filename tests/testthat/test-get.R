02c7701d33bd2aaddd7996a339c4826c52267c5a
kylebennison <kylebeni012@gmail.com>
2022-07-05 21:58:20 -0400

    fix test

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -1,4 +1,4 @@
 test_that("Test that get returns a dataframe object with data", {
-  expect_type(get_plays(), "data.frame")
+  expect_s3_class(get_plays(), "data.frame", exact = FALSE)
   expect_gt(nrow(get_plays()), 0L)
 })
