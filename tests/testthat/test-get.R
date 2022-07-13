187575b5ada77d63fb519e6e68a7a545b940dd2f
kylebennison <kylebeni012@gmail.com>
2022-07-13 08:40:03 -0400

    simplify test names

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -1,18 +1,18 @@
-test_that("Test that get_plays() returns a dataframe object with data", {
+test_that("get_plays() works", {
   expect_s3_class(get_plays(), "data.frame", exact = FALSE)
   expect_gt(nrow(get_plays()), 0L)
 })
 
-test_that("Test that colors function works", {
+test_that("get_colors() function works", {
   expect_s3_class(get_colors(), "data.frame", exact = FALSE)
 })
 
-test_that("Test that get_games() returns a dataframe object with data", {
+test_that("get_games() works", {
   expect_s3_class(get_games(), "data.frame", exact = FALSE)
   expect_gt(nrow(get_games()), 0L)
 })
 
-test_that("Test that get_anything() returns a dataframe object with data", {
+test_that("get_anything() works", {
   expect_s3_class(
     get_anything(url = "https://api.collegefootballdata.com/venues"),
     "data.frame",
@@ -23,17 +23,17 @@ test_that("Test that get_anything() returns a dataframe object with data", {
   ), 0L)
 })
 
-test_that("Test that get_betting() returns a dataframe object with data", {
+test_that("get_betting() works", {
   expect_s3_class(get_betting(), "data.frame", exact = FALSE)
   expect_gt(nrow(get_betting()), 0L)
 })
 
-test_that("Test that get_drives() returns a dataframe object with data", {
+test_that("get_drives() works", {
   expect_s3_class(get_drives(), "data.frame", exact = FALSE)
   expect_gt(nrow(get_drives()), 0L)
 })
 
-test_that("Test that get_elo() returns a dataframe object with data", {
+test_that("get_elo() works", {
   expect_s3_class(get_elo(), "data.frame", exact = FALSE)
   expect_gt(nrow(get_elo()), 0L)
 })
